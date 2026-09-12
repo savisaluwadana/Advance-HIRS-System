eac064d661925ca77320a6dd621af7ff5d9c267b
savi saluwadana <82082138+savisaluwadana@users.noreply.github.com>
2026-09-12 12:10:52 +0530

    feat: enable HR workflow API routes

--- apps/api/cmd/server/main.go
+++ apps/api/cmd/server/main.go
@@ -44,7 +44,7 @@ func main() {
 	port := envOr("API_PORT", "8080")
 	server := &http.Server{
 		Addr:              ":" + port,
-		Handler:           httpapi.NewHandler(dataStore, authManager, envOr("WEB_ORIGIN", "http://localhost:3000")),
+		Handler:           httpapi.NewHandlerV2(dataStore, authManager, envOr("WEB_ORIGIN", "http://localhost:3000")),
 		ReadHeaderTimeout: 5 * time.Second,
 		ReadTimeout:       15 * time.Second,
 		WriteTimeout:      30 * time.Second,
